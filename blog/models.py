4593f8271f64bdbff448eedef5ca75cad7b16bfb
Marcelo Canina <me@marcanuy.com>
2019-02-15 11:52:23 -0300

    add unique slug example

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
+import itertools
+
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -8,8 +11,38 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('article-detail', args=[str(self.id)])
 
+class ArticleUniqueSlug(Article):
+
+    slug = models.SlugField(
+        default='',
+        editable=False,
+        max_length=100, #random length
+    )
+
+    def get_absolute_url(self):
+        kwargs = {
+            'slug': self.slug
+        }
+        return reverse('articleunique-slug', kwargs=kwargs)
+
+
+    def save(self, *args, **kwargs):
+        max_length = self._meta.get_field('slug').max_length
+        value = self.title
+        slug_candidate = slug_original = slugify(value, allow_unicode=True)
+        for i in itertools.count(1):
+            if not ArticleUniqueSlug.objects.filter(slug=slug_candidate).exists():
+                break
+            slug_candidate = '{}-{}'.format(slug_original, i)
+
+        self.slug = slug_candidate
+        super().save(*args, **kwargs)
+
+
 class ArticlePkAndSlug(models.Model):
-    title = models.CharField(max_length=settings.BLOG_TITLE_MAX_LENGTH)
+    title = models.CharField(
+        max_length=settings.BLOG_TITLE_MAX_LENGTH
+    )
     slug = models.SlugField(
         default='',
         editable=False,
@@ -22,3 +55,10 @@ class ArticlePkAndSlug(models.Model):
             'slug': self.slug
         }
         return reverse('article-pk-slug-detail', kwargs=kwargs)
+
+    def save(self, *args, **kwargs):
+        value = self.title
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,45 @@
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 
-from blog.models import Article, ArticlePkAndSlug
+from blog.models import Article, ArticlePkAndSlug, ArticleUniqueSlug
 
 class ArticleDetailView(DetailView):
-
     model = Article
-
+    
+    
 class ArticleListView(ListView):
 
     model = Article
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articlepkandslug_list'] = ArticlePkAndSlug.objects.all()
+        context['articleuniqueslug_list'] = ArticleUniqueSlug.objects.all()
+
+        return context
 
 class ArticleCreateView(CreateView):
     model = Article
     fields = ['title']
 
+    success_url = reverse_lazy('article-list')
 
-class ArticlePkAndSlug(DetailView):
+    def form_valid(self, form):
+        title = form.cleaned_data.get('title')
+        # create pk and slug model article
+        ArticlePkAndSlug(title=title).save()
+        # create unique slug article
+        ArticleUniqueSlug(title=title).save()
+        return super().form_valid(form)
+
+class ArticlePkAndSlugDetailView(DetailView):
     model = ArticlePkAndSlug
+    query_pk_and_slug = True
+    template_name = 'blog/article_detail.html'
+
+class ArticleUniqueSlugDetailView(DetailView):
+    model = ArticlePkAndSlug
+    query_pk_and_slug = False
+    template_name = 'blog/article_detail.html'
