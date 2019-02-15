dcee91aa5bbf542ec2f8f524937c7ee5762f388c
Marcelo Canina <me@marcanuy.com>
2019-02-15 09:57:53 -0300

    add article url in template list

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.urls import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('article-detail', args=[str(self.id)])
