3add859ee0237a9cacb83b2c18e02d8c103f45b2
Marcelo Canina <me@marcanuy.com>
2020-08-30 09:44:54 -0300

    show title max length setting in template

---
+++ blog/context_processors.py
@@ -0,0 +1,15 @@
+from django.conf import settings as django_settings
+
+
+def site_settings(request):
+    """Expose specific settings to templates"""
+    result = {}
+    settings = [
+        "BLOG_TITLE_MAX_LENGTH",
+    ]
+    for attr in settings:
+        if (hasattr(django_settings, attr)):
+            result[attr] = getattr(django_settings, attr)
+    return {
+        'settings': result,
+    }
